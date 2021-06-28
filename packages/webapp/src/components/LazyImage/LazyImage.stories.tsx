3503635b4de4bd3a8e88fbece94042603c25aefb
JimZer <jimi.vaubien@protonmail.com>
2021-06-28 15:00:02 +0200

    LazyImage component for faster rendering

---
+++ packages/webapp/src/components/LazyImage/LazyImage.stories.tsx
@@ -0,0 +1,20 @@
+import { Box } from '@chakra-ui/react';
+import { Story } from '@storybook/react';
+
+import LazyImage, { LazyImageProps } from '.';
+
+export default {
+  title: 'LazyImage',
+  component: LazyImage
+};
+
+const Template: Story<LazyImageProps> = (arguments_) => (
+  <Box>
+    <LazyImage height="400px" {...arguments_} />
+  </Box>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  src: 'https://wallpapercave.com/wp/wp4676582.jpg'
+};
---
+++ packages/webapp/src/components/LazyImage/index.tsx
@@ -0,0 +1,39 @@
+import { Box, Image, ImageProps, useBoolean } from '@chakra-ui/react';
+import { Skeleton } from 'components/Skeleton';
+import delay from 'lodash/delay';
+import React from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
+
+export interface LazyImageProps extends ImageProps {
+  substitute?: JSX.Element;
+  substituteHeight?: number | string;
+}
+
+export default function LazyImage({
+  substitute,
+  substituteHeight,
+  ...props
+}: LazyImageProps): JSX.Element {
+  const [isLoaded, setIsLoaded] = useBoolean();
+  const [isVisible, setIsVisible] = useBoolean();
+
+  // FIXME - let the placeholder some time
+  const load = () => delay(() => setIsLoaded.on(), 500);
+
+  const placeholder = substitute ?? (
+    <Skeleton height={(substituteHeight ?? props.height ?? props.h ?? 'auto') as number | string} />
+  );
+
+  return (
+    <>
+      <VisibilitySensor onChange={(isVisible: boolean) => isVisible && setIsVisible.on()}>
+        <>
+          {!isLoaded && placeholder}
+          <Box as="span" display={isLoaded ? 'block' : 'none'}>
+            {isVisible && <Image onLoad={load} {...props} />}
+          </Box>
+        </>
+      </VisibilitySensor>
+    </>
+  );
+}
