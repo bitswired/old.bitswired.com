0c7e0648968440e89817cfb5a74e25350e542896
JimZer <jimi.vaubien@protonmail.com>
2021-07-02 01:18:11 +0200

    Add some constants

--- packages/webapp/src/utils/static-urls.ts
+++ packages/webapp/src/utils/static-urls.ts
@@ -1,5 +1,11 @@
 const BASE_IMG_OPTI = 'https://bitswired.fra1.digitaloceanspaces.com/images-opti';
 
-export default function (type: string, asset: string): string {
+export default function resolveStaticAsset(type: string, asset: string): string {
   return type === 'blog' ? `${BASE_IMG_OPTI}/${asset}@blog.webp` : `${BASE_IMG_OPTI}/${asset}`;
 }
+
+export const IMAGE_HOME = resolveStaticAsset('', 'home.webp');
+
+export const IMAGE_HOME_MOBILE = resolveStaticAsset('', 'virtual-reality.webp');
+
+export const IMAGE_WIREUP = resolveStaticAsset('', 'wireup.webp');
