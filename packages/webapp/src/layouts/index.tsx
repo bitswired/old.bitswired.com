5128fe6bfa363fc3289fdc27d765876db2c3c1fc
JimZer <jimi.vaubien@protonmail.com>
2021-07-02 01:17:00 +0200

    Add the newsletter modal globally for performance

--- packages/webapp/src/layouts/index.tsx
+++ packages/webapp/src/layouts/index.tsx
@@ -1,15 +1,21 @@
 import { Box, useDisclosure } from '@chakra-ui/react';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
+import NewsletterModal from 'components/Newsletter/NewsletterModal';
+import { NewsletterContext } from 'context/newsletter';
 import useHeaderVisible from 'hooks/header-visible';
 import React from 'react';
 
 interface MainLayoutProps {
   children: JSX.Element[] | JSX.Element;
 }
-export function MainLayout({ children }: MainLayoutProps): JSX.Element {
+export function MainLayout({ children }: MainLayoutProps): JSX.Element | null {
   const { isGoingDown, lock } = useHeaderVisible();
   const { isOpen: isSliderOpen, onOpen: onSliderOpen, onClose: onSliderClose } = useDisclosure();
+
+  const newsletterContext = React.useContext(NewsletterContext);
+  if (!newsletterContext) return null;
+  const { isOpen: isNewsletterModalOpen, close } = newsletterContext;
 
   return (
     <>
@@ -22,6 +28,7 @@ export function MainLayout({ children }: MainLayoutProps): JSX.Element {
       />
       <Box>{children}</Box>
       <Footer />
+      <NewsletterModal isOpen={isNewsletterModalOpen} onClose={close} onSubmit={() => null} />
     </>
   );
 }
