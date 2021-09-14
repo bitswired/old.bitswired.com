80e62c8bc1dbb090b31a28f03cb1c936c82cc6a1
JimZer <jimi.vaubien@protonmail.com>
2021-09-15 00:42:38 +0200

    Add some components to MDX

--- packages/webapp/src/components/MDX/index.tsx
+++ packages/webapp/src/components/MDX/index.tsx
@@ -1,12 +1,19 @@
 import {
+  Alert,
+  AlertIcon,
   AspectRatio,
   Box,
   Center,
   Code,
   CodeProps,
+  Flex,
+  Heading,
   Link,
   LinkProps,
-  ResponsiveValue
+  ResponsiveValue,
+  VStack,
+  Wrap,
+  WrapItem
 } from '@chakra-ui/react';
 import CodeBlock, { CodeBlockProps } from 'components/CodeBlock';
 import LazyImage from 'components/LazyImage';
@@ -76,7 +83,49 @@ function MDXInlineCodeBlock({ children }: CodeProps): JSX.Element {
   return <Code bgColor="#EEE">{children}</Code>;
 }
 
+interface InfoWarnSectionProps {
+  children: JSX.Element;
+}
+
+function InfoWarnSection({ children }: InfoWarnSectionProps): JSX.Element {
+  return (
+    <Flex direction={['column', 'row']} w="100%" justify="space-between">
+      {children}
+    </Flex>
+  );
+}
+
+interface InfoWarnBlockProps {
+  children: string;
+}
+
+function InfoWarnBlockBuilder(type: 'info' | 'warning') {
+  return function InfoWarnBlock({ children }: InfoWarnBlockProps) {
+    const title = type === 'info' ? 'Application' : 'Pitfalls';
+    return (
+      <Box w={['100%', '49%']}>
+        <Heading as="h2">{title}</Heading>
+
+        <VStack>
+          {children.split('&').map((text: string) => (
+            <Alert key="text" status={type}>
+              <AlertIcon />
+              {text.trim()}
+            </Alert>
+          ))}
+        </VStack>
+      </Box>
+    );
+  };
+}
+
 export const mdxComponents = {
+  InfoWarnSection: InfoWarnSection,
+  InfoBlock: InfoWarnBlockBuilder('info'),
+  WarnBlock: InfoWarnBlockBuilder('warning'),
+  Wrap: Wrap,
+  VStack: VStack,
+  WrapItem: WrapItem,
   Figure: MDXImage,
   code: MDXCodeBlock,
   inlineCode: MDXInlineCodeBlock,
