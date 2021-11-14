aba7c562cda5c93e950f07f1ee5ee7811b72e645
JimZer <jimi.vaubien@protonmail.com>
2021-11-14 20:52:07 +0100

    Rework article

---
+++ src/content/bitsof-python-builtin-caching/meta.ts
@@ -0,0 +1,14 @@
+export default {
+  title: 'Bits of Python - Built-in Caching',
+  description:
+    'Easily squeeze performance out of your scripts, with only slight modifications. Learn how to leverage caching easily in Python.',
+  readMinutes: 10,
+  image: 'https://statics.bitswired.com/images-opti/blog/gears.webp',
+  category: 'Programming',
+  tags: ['Python', 'Caching'],
+  slug: 'bitsof-python-builtin-caching',
+  published: true,
+  datePublished: '2019-01-01',
+  dateModified: '2019-01-01',
+  images: 'https://statics.bitswired.com/images-opti/blog/gears.webp'
+};
