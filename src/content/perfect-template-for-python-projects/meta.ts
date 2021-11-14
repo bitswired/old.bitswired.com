36fa63e743c922ad32ca88260ce206ef54d1daa1
JimZer <jimi.vaubien@protonmail.com>
2021-11-14 20:51:29 +0100

    Rework articles for new mdx structure

---
+++ src/content/perfect-template-for-python-projects/meta.ts
@@ -0,0 +1,14 @@
+export default {
+  title: 'Perfect Template to Start Python Projects',
+  description:
+    'Python is great but lacks structure. I detail the issues and provide a GitHub template repository to solve the issue.',
+  readMinutes: 20,
+  image: 'https://statics.bitswired.com/images-opti/blog/code.webp',
+  category: 'Programming',
+  tags: ['Python', 'Template', 'Best-Practices'],
+  slug: 'perfect-template-for-python-projects',
+  published: true,
+  datePublished: '2020-01-01',
+  dateModified: '2020-01-01',
+  images: ['https://statics.bitswired.com/images-opti/blog/code.webp']
+};
